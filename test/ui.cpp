485cf04c3c178e16ff44bf19104b91d35d608503
IamJomm <artemlantsman05@gmail.com>
2024-05-14 23:39:59 +0300

    Update

--- test/ui.cpp
+++ test/ui.cpp
@@ -1,16 +1,21 @@
 #include <ncurses.h>
 
+#include <chrono>
 #include <clocale>
+#include <cmath>
+#include <cstdio>
 #include <string>
+#include <thread>
 #include <vector>
 
 using namespace std;
+namespace ch = std::chrono;
 
 class ui {
    public:
     WINDOW* win = nullptr;
 
-    short drawmenu(const string& title, const vector<string>& choices) {
+    short menu(const string& title, const vector<string>& choices) {
         noecho();
         curs_set(false);
 
@@ -57,28 +62,85 @@ class ui {
             wattroff(win, A_STANDOUT);
         } while ((input = wgetch(win)) != '\n');
 
+        clear();
         echo();
         curs_set(true);
         return choice;
     }
+
+    void progressBar(const long& prog, const long& total) {
+        short y, x;
+        getyx(win, y, x);
+        const int len = 30;
+        short percent = ceil(float(len) / total * prog);
+        wchar_t str[len + 1];
+        for (short i = 0; i < percent; i++) str[i] = L'\u2588';
+        for (short i = percent; i < len; i++) str[i] = L'\u2592';
+        str[len] = L'\0';
+        mvwprintw(win, y, 1, "|%ls|", str);
+        if (prog == total) wmove(win, y + 1, 1);
+        wrefresh(win);
+    }
+
+    void notification(const string& title, const vector<string>& message) {
+        noecho();
+        curs_set(false);
+        short maxWidth = 0, maxHeight = message.size(), y, x;
+        for (const string& str : message) {
+            short len = str.length();
+            if (len > maxWidth) maxWidth = len;
+        }
+        short titleLen = title.length();
+        if (titleLen > maxWidth) maxWidth = titleLen;
+        maxWidth += 4;
+        getmaxyx(stdscr, y, x);
+        WINDOW* ntfwin =
+            newwin(maxHeight + 2, maxWidth, y / 2 - (maxHeight + 2) / 2,
+                   x / 2 - maxWidth / 2);
+        box(ntfwin, 0, 0);
+        mvwprintw(ntfwin, 0, maxWidth / 2 - titleLen / 2, "%s", title.c_str());
+        for (short i = 0; i < maxHeight; i++)
+            mvwprintw(ntfwin, i + 1, maxWidth / 2 - message[i].length() / 2,
+                      "%s", message[i].c_str());
+        wrefresh(ntfwin);
+        wgetch(ntfwin);
+        werase(ntfwin);
+        wrefresh(ntfwin);
+        delwin(ntfwin);
+        echo();
+        curs_set(true);
+    }
+
     ui() {
         setlocale(LC_ALL, "");
         initscr();
         cbreak();
-        int y, x;
-        getyx(stdscr, y, x);
-        win = newwin(x, y, 0, 0);
+        short y, x;
+        getmaxyx(stdscr, y, x);
+        win = newwin(y, x, 0, 0);
         scrollok(win, true);
         keypad(win, true);
         box(win, 0, 0);
         refresh();
         wrefresh(win);
     }
-    ~ui() { endwin(); }
+    ~ui() {
+        delwin(win);
+        endwin();
+    }
 };
 
 int main() {
     ui ui;
-    ui.drawmenu("Kurwa:", {"sex", "boobies", "AAA"});
+    // ui.menu("Kurwa:", {"sex", "boobies", "AAA"});
+    ui.notification("KURWA", {"SEEEEEEEEEEEEEEEEEEX",
+                              "SEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEX", "SEX"});
+    int n = 29;
+    for (int i = 0; i < 30; i++) {
+        for (int j = 0; j <= n; j++) {
+            this_thread::sleep_for(ch::milliseconds(10));
+            ui.progressBar(j, n);
+        }
+    }
     getch();
 }
